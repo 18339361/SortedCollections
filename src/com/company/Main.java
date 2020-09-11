f69bead738debf71b7b1a09b3f78c014d19291ce
18339361 <pranchal2001@gmail.com>
2020-09-11 21:07:12 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,100 @@
+package com.company;
+
+import org.w3c.dom.html.HTMLImageElement;
+
+import java.util.Map;
+
+public class Main {
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 0.86, 100);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cake", 1.10, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.50, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.00, 10);
+        stockList.addStock(temp);
+
+        //we can add same thing more than once
+        temp = new StockItem("cup", 0.50, 200);
+        stockList.addStock(temp);
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.50, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        for(String s : stockList.Items().keySet()){
+            System.out.println(s);
+        }
+
+        Basket timsBasket = new Basket("Tim");
+        sellItem(timsBasket, "car", 1);
+        System.out.println(timsBasket);
+
+        sellItem(timsBasket, "car", 1);
+        System.out.println(timsBasket);
+
+        //making sure cars are in stock before we sell one
+        if(sellItem(timsBasket, "car", 1) != 1){
+            System.out.println("There are no more cars in stock");
+        }
+
+        sellItem(timsBasket, "spanner", 5); //does not exist
+        System.out.println(timsBasket);
+
+        sellItem(timsBasket, "juice", 4);
+        sellItem(timsBasket, "cup", 12);
+        sellItem(timsBasket, "bread", 1);
+        System.out.println(timsBasket);
+
+        System.out.println(stockList);
+
+        //gives error because it returns an unmodifiable map i.e you can only read from it
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+
+        //individual items can be modified but map can't
+        stockList.Items().get("car").adjustStock(2000);
+        stockList.get("car").adjustStock(-1000);
+        System.out.println(stockList);
+
+        //neither individual items nor the map can be modified
+        for(Map.Entry<String, Double> price : stockList.priceList().entrySet()){
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity){
+        //retrieve the item from the stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null){
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(stockList.sellStock(item, quantity) != 0){
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
+    }
+}
